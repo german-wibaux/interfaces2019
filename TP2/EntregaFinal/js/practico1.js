65d364961bd3ce38a95fdfe5c27bd7fe09619ec6
Yerman <germanwibaux@gmail.com>
2019-09-10 21:12:00 -0300

    Add TP2

---
+++ TP2/EntregaFinal/js/practico1.js
@@ -0,0 +1,50 @@
+
+
+// Para definir una matriz en cada posicion se crear un nuevo array
+// y asi obtener la matriz como array de arrays
+
+// cargar imagen
+
+// degrade con colores
+
+// primera mitad se decrece el r y el g
+
+// y para la segunda mitad se decrece el g
+
+// cargar imagen
+
+// let image = new Image();
+// image.src = "img/proove.png";
+
+// image.onload = function () {
+//     drawImageMethod (this);
+// }
+
+// function drawImageMethod(image) {
+//     context.drawImage(image,0,0);
+// }
+
+
+// 
+// Para el del degrade hay que dividir la altura o el largo i o j por la altura total y el ressultado
+// multiplicarlo por 255 para obtener el color
+// para  el ejercicio de cargar una imagen tener en cuenta calcar la imagen
+// y usar el onload para que se pueda tocar una vez que esta cargada
+// y para aplicar un filtro obtener los valores de r, g y b
+// y elegir una tecnica para dejarlos igual y por ejemplo aplicar un filtro de
+// gris
+// console.table(principal_mat);
+
+// cuestiones de mejora de entrega
+// hacer que la imagen se vuelva el alto y el ancho del canvas
+// que la imagen tome las dimensiones del canvas
+// clear rec o fill rec para limpiar el canvas
+
+// alert( print_array() );
+// console.log ( avg_rows() );
+// console.log("1 is " + isOdd(1));
+// console.log("2 is " + isOdd(2));
+// console.log("3 is " + isOdd(3));
+// console.log("4 is " + isOdd(4));
+
+// console.log(principal_mat[99] [99]);
