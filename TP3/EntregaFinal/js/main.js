3f5b7ba639a0ab7a0f555589fa06af5647e28228
Yerman <germanwibaux@gmail.com>
2019-09-25 19:21:00 -0300

    Init game logic

--- TP3/EntregaFinal/js/main.js
+++ TP3/EntregaFinal/js/main.js
@@ -1,192 +1,31 @@
 "use strict"
 
+document.addEventListener('keypress', jump);
 
-window.onload = function() {
-    let avgHour
-    let avgMin
-    let avgSeg
-
-    // let gameArea = {
-    //     canvas: document.createElement("canvas"),
-    //     start: function() {
-    //         this.canvas.width = window.innerWidth;
-    //         this.canvas.height = window.innerHeight;
-    //         this.context = this.canvas.getContext("2d");
-    //         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    //         this.interval = setInterval(updateGameArea, 20);
-    //     },
-    //     clear: function() {
-    //         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    //     }
-    // }
-
-    // console.log(document.body.childNodes[0]);
-
-    let gameCanvas = document.getElementById("picture")
-    let ctx = document.getElementById("picture").getContext("2d")
-        // gameCanvas.width = document.getElementById("con-sprite").offsetWidth
-        // gameCanvas.height = document.getElementById("con-sprite").offsetHeight
-
-    // var gameCanvas = document.createElement('canvas')
-
-    // gameCanvas.id = "game"
-    // gameCanvas.width = width
-    // gameCanvas.height = heigth
-    // gameCanvas.style.zIndex = 8
-    // gameCanvas.style.position = "relative"
-
-    // var height = canvas.height = window.innerHeight
-    // var width1 = canvas.width = window.innerWidth
-
-    // let canvas = document.getElementById("picture");
-    // let context = document.getElementById("picture").getContext("2d");
-
-    // function clock() {
-    // 	let time = new Date();
-    // 	let hour = time.getHours();
-    // 	let min = time.getMinutes();
-    // 	let seg = time.getSeconds();
-
-    //     if (hour >= 12) {
-    //      	let avgHour = hour / 12 * 360;
-    //     }  else {
-    //     	let avgHour = hour / 24 * 360;
-    //     }
-
-    //     avgHour += min / 60 * 30;
-    //     avgMin = min / 60 * 360;
-    //     avgSeg = seg / 60 * 360;
-
-    //     document.getElementById("hour").style.transform = "rotate("+ avgHour +"deg)";
-    //     document.getElementById("min").style.transform = "rotate("+ avgMin +"deg)";
-    //     document.getElementById("seg").style.transform = "rotate("+ avgSeg +"deg)";
-    // }
+let move = document.getElementById('move')
+let back = document.getElementById('con-sprite')
+move.onclick = run
 
 
-    // document.getElementById("seg").style.transform = "rotate(90deg)";
-    // let rect1 = new Rect(0, 0, 50, 50, "#ffffff", context);
-    // setInterval(clock, 1000);
-    const root = document.documentElement;
-    // document.onmousemove = move
-    let rect3 = document.getElementsByClassName('rect');
 
-    function move(e) {
 
-        // tell the browser we're handling this mouse event
-        e.preventDefault()
-        e.stopPropagation()
-            // let position = getPosition(e)
-            // position = getPosition(e)
 
-        // rect1.changePosition(position[0], position[1], context)
-        let x = e.clientX / innerWidth;
-        let y = e.clientY / innerHeight;
-        console.log("paso")
-        root.style.setProperty('--mouse-x', x)
-        root.style.setProperty('--mouse-y', y)
-
+function jump(e) {
+    if (e.keyCode == 32) {
+        let run = document.getElementById('quiet')
+        run.className = 'reaper-jump'
     }
+}
 
-
-
-
-    // rect.onmousedown = myDownClick
-
-    function myDownClick() {
-        changeColor(rect)
-        changeScale(rect)
-        changePosition(rect)
-
-
+function jumpNot(e) {
+    if (e.keyCode == 32) {
+        let run = document.getElementById('quiet')
+        run.className = 'reaper'
     }
+}
 
-    // function changeScale(ball) {
-    //     for (let i = 0; i < 2; i++) {
-    //         let width = Math.floor((Math.random() * 200) + 10);
-    //         let height = Math.floor((Math.random() * 200) + 10);
-    //         rect.style.width = width + "px"
-    //         rect.style.height = height + "px"
-    //     }
-    // }
-
-    function changePosition(ball) {
-        for (let i = 0; i < 2; i++) {
-            let top = Math.floor((Math.random() * 200) + 10);
-            let left = Math.floor((Math.random() * 200) + 10);
-            rect.style.top = top + "px"
-            rect.style.left = left + "px"
-        }
-    }
-
-
-
-    // function chanScale() {
-    //     let scale = ""
-    //     for (let i = 0; i < 2; i++) {
-    //         let sub = Math.floor(Math.random() * 256).toString(16)
-    //         color += (sub.length == 1 ? "0" + sub : sub)
-
-    //     }
-
-    // }
-
-    function getPosition(event) {
-        let rect = canvas.getBoundingClientRect();
-        let x = event.clientX - rect.left; // x == the location of the click in the document - the location (relative to the left) of the canvas in the document
-        let y = event.clientY - rect.top; // y == the location of the click in the document - the location (relative to the top) of the canvas in the document
-        return [x, y];
-    }
-
-    function getSetStyle(option) {
-        switch (option) {
-            case 0:
-                let setStyle = [
-                    [backgroundColor, "#333333"],
-                    [width, "8rem"],
-                    [heigth, "8rem"]
-                ];
-                return setStyle;
-            case 1:
-                alert("Bananan");
-                break;
-        }
-    }
-
-
-    function changeColor(ball) {
-        let color = "";
-        for (let i = 0; i < 3; i++) {
-            let sub = Math.floor(Math.random() * 256).toString(16)
-            color += (sub.length == 1 ? "0" + sub : sub)
-        }
-        rect.style.backgroundColor = "#" + color
-
-        //     let sub = Math.floor(Math.random() * 256).toString(8)
-        //     color += (sub.length == 1 ? "0" + sub : sub)
-
-        // let setStylus = getSetStyle(color);
-        // console.log(setStylus);
-        // for (let i = 0; i < setStylus.length; i) {
-
-        //     console.log(setStylus);
-        // }       
-
-
-    }
-
-
-
-    /**
-     
-     function get_random_color() 
-     {
-         var color = "";
-         for(var i = 0; i < 3; i++) {
-             var sub = Math.floor(Math.random() * 256).toString(16)
-             color += (sub.length == 1 ? "0" + sub : sub)
-         }
-         return "#" + color
-     }
-
-     */
+function run() {
+    let run = document.getElementById('quiet')
+    back.className = 'contain-sprite'
+    run.className = 'reaper'
 }
