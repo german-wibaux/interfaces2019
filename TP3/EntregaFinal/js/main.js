3cc7b5b102c181b3881668dc626d238d8272f527
Yerman <germanwibaux@gmail.com>
2019-09-25 22:03:31 -0300

    Jump action

--- TP3/EntregaFinal/js/main.js
+++ TP3/EntregaFinal/js/main.js
@@ -3,11 +3,18 @@
 document.addEventListener('keypress', jump);
 
 let move = document.getElementById('move')
+let quietButton = document.getElementById('quiet-button')
+let jumpButton = document.getElementById('jump-button')
 let back = document.getElementById('con-sprite')
 move.onclick = run
+quietButton.onclick = stop
+jumpButton.onclick = jumpTrans
 
-
-
+function stop () {
+    let quietBt = document.getElementById('quiet')
+    back.className = 'contain-sprite-quiet'
+    quietBt.className = 'quiet-reaper'
+}
 
 
 function jump(e) {
@@ -15,6 +22,12 @@ function jump(e) {
         let run = document.getElementById('quiet')
         run.className = 'reaper-jump'
     }
+}
+
+function jumpTrans() {
+    let jump = document.getElementById('quiet')
+    jump.className = 'reaper-jump'
+    setTimeout(run, 600);
 }
 
 function jumpNot(e) {
@@ -26,6 +39,7 @@ function jumpNot(e) {
 
 function run() {
     let run = document.getElementById('quiet')
+    run.style.animation = ''
     back.className = 'contain-sprite'
     run.className = 'reaper'
 }
